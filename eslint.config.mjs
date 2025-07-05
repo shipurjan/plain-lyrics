ed411207cf7fd7a1dada1aa462818de0a4257ebe
Cyprian Zdebski <shipurjan@gmail.com>
2025-07-05 02:49:48 +0200

    configure eslint & prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,54 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import js from '@eslint/js'
+import pluginNext from '@next/eslint-plugin-next'
+import eslintConfigPrettier from 'eslint-config-prettier/flat'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import pluginReact from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const config = tseslint.config([
+  {
+    ignores: [
+      'node_modules/**/*',
+      '.next/**/*',
+      'out/**/*',
+      '*.config.*[t|j]s',
+    ],
+  },
+  {
+    files: ['src/**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    plugins: { js },
+    extends: [js.configs.recommended],
+  },
+  {
+    files: ['src/**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    languageOptions: { globals: globals.browser },
+  },
+  {
+    plugins: {
+      '@next/next': pluginNext,
+    },
+  },
+  {
+    files: ['src/**/*.ts', 'src/**/*.tsx'],
+    // @ts-expect-error wrong type of next rules
+    rules: pluginNext.configs.recommended.rules,
+  },
+  eslintConfigPrettier,
+  eslintPluginPrettierRecommended,
+  pluginReact.configs.flat['jsx-runtime'],
+  reactHooks.configs['recommended-latest'],
+  tseslint.configs.strictTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+])
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
-export default eslintConfig;
+export default config
---
+++ prettier.config.mjs
@@ -0,0 +1,21 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import('prettier').Config}
+ */
+const config = {
+  // Remember to use `npm run format` to format the codebase when you change this file.
+  semi: false,
+  singleQuote: true,
+  plugins: ['@trivago/prettier-plugin-sort-imports'],
+  importOrder: [
+    '^react.*$',
+    '<THIRD_PARTY_MODULES>',
+    '^./(.*)$',
+    '^\\.\\/(.*)$',
+    '^\\.\\.\\/(.*)$',
+    '^@/(.*)$',
+  ],
+  importOrderSortSpecifiers: true,
+}
+
+export default config
